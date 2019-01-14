e64d0d3bdfec1f24b9322ed2b35752eefd10ae65
n.zarubin <n.zarubin@itsirius.ru>
2019-01-14 16:49:59 +0300

    master

---
+++ CurlTor.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Curl with TOR support
+     * User: n.zarubin
+     * Date: 14.01.2019
+     * Time: 16:48
+     */
+    namespace nikserg\yii2\CurlTor;
+    use linslin\yii2\curl;
+    class CurlTor extends curl\Curl {
+
+    }
